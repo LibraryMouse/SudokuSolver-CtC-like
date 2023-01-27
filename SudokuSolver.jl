5cfcf42fc502693f3055c2052dacf9d5f4b391ee
LibraryMouse <97201180+LibraryMouse@users.noreply.github.com>
2023-01-28 00:38:14 +0100

    Add files via upload
    Modified Cell type. Added Sudoku type

--- SudokuSolver.jl
+++ SudokuSolver.jl
@@ -1,5 +1,10 @@
-mutable struct Cell
-    values::Vector{Int64}
+struct Cell
+    candidates::Vector{Bool}
+    Cell() = new(ones(Bool, 9))
 end
 
 
+struct Sudoku
+    grid::Matrix{Cell}
+    Sudoku() = new(Matrix{Cell}(undef, 9, 9))
+end
